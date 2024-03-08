59255de7d592d75e4ffe105de6f8d23269f887f2
Nghi Nguyen Thanh <nghi.nguyen@evehr.vn>
2024-03-08 13:09:33 +0700

    feat: basis of testing with jest

--- Unit test with Jest/README.md
+++ Unit test with Jest/README.md
@@ -1,12 +1,17 @@
 # Unit Testing for Typescript & NodeJs Developers with Jest
 
+- [Unit Testing for Typescript \& NodeJs Developers with Jest](#unit-testing-for-typescript--nodejs-developers-with-jest)
+  - [1. Resources](#1-resources)
+  - [2. How to setup?](#2-how-to-setup)
+  - [3. How to test?](#3-how-to-test)
+    - [3.1 Test structure](#31-test-structure)
+    - [3.2 F.I.R.S.T principles](#32-first-principles)
+
 ## 1. Resources
 
 - [Udemy Link](https://www.udemy.com/course/unit-testing-typescript-nodejs/?couponCode=KEEPLEARNING)
 
 ## 2. How to setup?
-
-### 2.1 Install library
 
 ```bash
 yarn init -y
@@ -15,6 +20,16 @@ yarn add -D typescript ts-node jest ts-jest @jest/types @types/jest
 npx ts-jest config:init
 ```
 
-### 2.2 Test structure
+## 3. How to test?
+
+### 3.1 Test structure
 
 - arrange, actual, assert
+
+### 3.2 F.I.R.S.T principles
+
+- Fast
+- Independent
+- Repeatable
+- Self-validating
+- Thorough
--- Unit test with Jest/src/test/Utils.test.ts
+++ Unit test with Jest/src/test/Utils.test.ts
@@ -1,7 +1,7 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
-  test("should return uppercase of valid string", () => {
+  it("should return uppercase of valid string", () => {
     // arrange
     const sut = toUpperCase;
     const expected = "ABC";
@@ -11,5 +11,85 @@ describe("Utils test suite", () => {
 
     // assert
     expect(actual).toBe(expected);
+  });
+
+  describe.only("ToUpperCase examples", () => {
+    it.each([
+      {
+        input: "abc",
+        expected: "ABC",
+      },
+      {
+        input: "My-String",
+        expected: "MY-STRING",
+      },
+      {
+        input: "def",
+        expected: "DEF",
+      },
+    ])("$input toUpperCase should be $expected", ({ input, expected }) => {
+      const actual = toUpperCase(input);
+      expect(actual).toBe(expected);
+    });
+  });
+
+  describe("getStringInfo for arg My-String should", () => {
+    test("return right length", () => {
+      const sut = getStringInfo;
+      const actual = sut("My-String");
+      expect(actual.length).toBe(9);
+    });
+    test("return right lower case", () => {
+      const sut = getStringInfo;
+      const actual = sut("My-String");
+      expect(actual.lowerCase).toBe("my-string");
+    });
+    test("return right upper case", () => {
+      const sut = getStringInfo;
+      const actual = sut("My-String");
+      expect(actual.upperCase).toBe("MY-STRING");
+    });
+    test("return right length", () => {
+      const sut = getStringInfo;
+      const actual = sut("My-String");
+      expect(actual.length).toBe(9);
+      expect(actual).toHaveLength(9);
+    });
+    test("return right characters", () => {
+      const sut = getStringInfo;
+      const actual = sut("My-String");
+      expect(actual.characters).toEqual([
+        "M",
+        "y",
+        "-",
+        "S",
+        "t",
+        "r",
+        "i",
+        "n",
+        "g",
+      ]);
+      expect(actual.characters).toContain<string>("M");
+      expect(actual.characters).toEqual(
+        expect.arrayContaining(["S", "t", "r", "i", "n", "g", "M", "y", "-"])
+      ); // This method only content, do not check text orders
+    });
+    test("return defined extra info", () => {
+      const sut = getStringInfo;
+      const actual = sut("My-String");
+      expect(actual.extraInfo).toEqual({}); // toEqual applied for object
+      expect(actual.extraInfo).not.toBe(undefined);
+      expect(actual.extraInfo).not.toBeUndefined;
+      expect(actual.extraInfo).toBeDefined;
+      expect(actual.extraInfo).toBeTruthy;
+    });
+  });
+
+  it("should return info for valid string", () => {
+    // arrange
+    const sut = getStringInfo;
+
+    // act
+    const actual = sut("My-String");
   });
 });
