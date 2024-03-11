a2bbff1360a3c3fb7d3c72c5b2eba13432aaab53
Nghi Nguyen Thanh <nghi.nguyen@evehr.vn>
2024-03-11 11:17:57 +0700

    feat: test doubles in Jest

---
+++ Unit test with Jest/src/app/doubles/OtherUtils.ts
@@ -0,0 +1,50 @@
+import { v4 } from "uuid";
+
+export type stringInfo = {
+  lowerCase: string;
+  upperCase: string;
+  characters: string[];
+  length: number;
+  extraInfo: Object;
+};
+
+type LoggerServiceCallback = (arg: string) => void;
+
+export function toUpperCase(arg: string) {
+  return arg.toUpperCase();
+}
+
+export function toLowerCaseWithId(arg: string) {
+  return arg.toLowerCase() + v4();
+}
+
+export function calculateComplexity(stringInfo: stringInfo) {
+  return Object.keys(stringInfo.extraInfo).length * stringInfo.length;
+}
+
+export function toUpperCaseWithCb(
+  arg: string,
+  callback: LoggerServiceCallback
+) {
+  if (!arg) {
+    callback("Invalid argument!");
+    return;
+  }
+
+  callback(`called function with ${arg}`);
+  return arg.toUpperCase();
+}
+
+export class OtherStringUtils {
+  private callExternalService() {
+    console.log("Calling external service!!!");
+  }
+
+  public toUpperCase(arg: string) {
+    return arg.toUpperCase();
+  }
+
+  public logString(arg: string) {
+    console.log(arg);
+  }
+}
